f30cd4170abe2797b224b8fe83259b392f6b646c
Toan Do <kaido4492@gmail.com>
2026-03-23 16:12:28 +0700

    chore: release v1.24.0

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -12,7 +12,7 @@ import (
 	"govard/internal/ui"
 )
 
-var Version = "1.23.0"
+var Version = "1.24.0"
 
 var verbose bool
 
--- internal/desktop/app.go
+++ internal/desktop/app.go
@@ -24,7 +24,7 @@ func (app *App) GetUserInfo() (UserInfo, error) {
 	return res, nil
 }
 
-var Version = "1.23.0"
+var Version = "1.24.0"
 
 type App struct {
 	ctx context.Context
