bf143c94897c48132b440e794b48564605496598
Toan Do <kaido4492@gmail.com>
2026-03-30 16:58:49 +0700

    chore: release v1.37.1

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "1.37.0"
+var Version = "1.37.1"
 
 var verbose bool
 
--- internal/desktop/app.go
+++ internal/desktop/app.go
@@ -28,7 +28,7 @@ func (app *App) GetUserInfo() (res UserInfo, err error) {
 	return res, nil
 }
 
-var Version = "1.37.0"
+var Version = "1.37.1"
 
 type App struct {
 	ctx context.Context
